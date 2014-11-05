aa067e64e865b56ad491d75245fb65df48d3a245
Manuel Hohmann <mhohmann@physnet.uni-hamburg.de>
2014-11-05 02:19:50 +0200

    Add simple XSLT parser script.

---
+++ base/php/view.php
@@ -0,0 +1,28 @@
+<?php
+
+// Check whether the requested XML data generator exists.
+if(!file_exists("views/" . $_GET['view'] . ".php"))
+	die("View file " . $_GET['view'] . ".php not found.");
+
+// Include it and generate XML data.
+include("views/" . $_GET['view'] . ".php");
+$xml = $xmlgen->generateXML();
+if(!$xml->schemaValidate("../xml/osgdata.xsd"))
+	die("ERROR: Invalid XML generated!");
+
+// Load XSLT style.
+$xsl = new DOMDocument;
+$xsl->resolveExternals = true;
+$xsl->substituteEntities = true;
+$xsl->load("../../styles/xslt/default/view.xsl");
+
+// Transform data to HTML.
+$proc = new XSLTProcessor;
+$proc->importStyleSheet($xsl);
+$output = $proc->transformToXML($xml);
+
+// Output data.
+header("Content-Type: text/html; charset=UTF-8");
+echo $output;
+
+?>
