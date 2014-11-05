bf4afcc615a2d6c79e67a6ea0c359436716c9f0f
Manuel Hohmann <mhohmann@physnet.uni-hamburg.de>
2014-11-05 02:20:30 +0200

    Add simple mockup building view.

---
+++ base/php/views/buildings.php
@@ -0,0 +1,36 @@
+<?php
+
+class buildingsXML
+{
+	public function generateXML()
+	{
+		$time = time();
+		$xml = new DomDocument();
+		$xml->loadXML(<<<EOD
+<?xml version="1.0" encoding="utf-8"?>
+<view lang="en" servertime="$time" session="1234567890abcdef">
+<positions curg="1" curs="1" curo="1" curc="1">
+<position name="Thanata" galaxy="1" solsys="1" orbit="1" celb="1" tid="wat" tname="Water planet"/>
+<position name="Jolanthea" galaxy="2" solsys="2" orbit="2" celb="2" tid="gas" tname="Gas planet"/>
+</positions>
+<resources>
+<resource id="tit" name="Titanium" present="3600" storage="10000" produced="100"/>
+<resource id="sil" name="Silicon" present="2000" storage="4000" produced="40"/>
+<resource id="hyd" name="Hydrogen" present="500" storage="1000" produced="10"/>
+</resources>
+<buildings>
+<building id="titm" name="Titanium mine" time="3600" curlevel="2" planlevel="2">
+<cost id="tit" value="1800"/>
+<cost id="sil" value="900"/>
+</building>
+</buildings>
+</view>
+EOD
+);
+		return $xml;
+	}
+}
+
+$xmlgen = new buildingsXML();
+
+?>
