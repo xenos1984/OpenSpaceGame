5c0feec379a4cdd909164799c85cab761ece94df
Manuel Hohmann <mhohmann@physnet.uni-hamburg.de>
2014-11-10 22:39:49 +0200

    Add buildings to database.

--- base/php/install/creategame.php
+++ base/php/install/creategame.php
@@ -3,6 +3,7 @@ include_once("config.php");
 include_once("class/db.php");
 include_once("class/trans.php");
 include_once("class/resource.php");
+include_once("class/building.php");
 
 echo "<h3>Create universe configuration</h3>\n";
 
@@ -45,6 +46,57 @@ foreach($ress as $res)
 	$storage = $res->getAttribute("storage");
 
 	resource::create($id, $value, $product, $storage);
+}
+echo '<span style="color:lime">Success.</span></li>' . "\n";
+
+echo "<li>Create buildings... ";
+$buildings = $xpath->query("/game/building");
+foreach($buildings as $building)
+{
+	$id = $building->getAttribute("id");
+	$time = $building->getAttribute("time");
+	$factor = $building->getAttribute("factor");
+
+	building::create($id, $time, $factor);/*
+	$build = new building($building_ids[$idName]);
+
+	$buildons = $xpath->query("buildon", $building);
+	foreach($buildons as $buildon)
+	{
+		$orbType = $buildon->getAttribute("id");
+
+		$build->create_build_on($orb_ids[$orbType]);
+	}
+
+	$costs = $xpath->query("cost", $building);
+	foreach($costs as $cost)
+	{
+		$ressId = $cost->getAttribute("id");
+		$value = $cost->getAttribute("value");
+		$factor = $cost->getAttribute("factor");
+
+		$build->create_cost($ress_ids[$ressId], $value, $factor);
+	}
+
+	$products = $xpath->query("product", $building);
+	foreach($products as $product)
+	{
+		$ressId = $product->getAttribute("id");
+		$value = $product->getAttribute("value") / 3600.0;
+		$power = $product->getAttribute("power");
+
+		$build->create_product($ress_ids[$ressId], $value, $power);
+	}
+
+	$storages = $xpath->query("storage", $building);
+	foreach($storages as $storage)
+	{
+		$ressId = $storage->getAttribute("id");
+		$value = $storage->getAttribute("value");
+		$power = $storage->getAttribute("power");
+
+		$build->create_storage($ress_ids[$ressId], $value, $power);
+	}*/
 }
 echo '<span style="color:lime">Success.</span></li>' . "\n";
 
