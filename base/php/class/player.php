63a52870520d5b2405f5e6445bd1985163eee011
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2014-11-26 22:40:39 +0200

    Added player class.

---
+++ base/php/class/player.php
@@ -0,0 +1,26 @@
+<?php
+	include_once("class/db.php");
+
+	class player
+	{
+		public static function create($nick, $password, $email)
+		{
+			$pwhash = password_hash($password, PASSWORD_DEFAULT);
+
+			return db::insert('players', array(
+				'nick' => $nick,
+				'pwhash' => $pwhash,
+				'email' => $email));
+		}
+
+		public static function checkpass($nick, $password)
+		{
+			$data = db::select_one('players', array('id', 'pwhash'), array('nick' => $nick));
+			if(!$data)
+				return false;
+			if(!password_verify($password, $data['pwhash']))
+				return false;
+			return $data['id'];
+		}
+	}
+?>
