77790ed50cee00c78917568542d0529494039c09
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2014-11-26 22:57:33 +0200

    Added simple start page.

---
+++ base/php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>OpenSpaceGame</title>
+	</head>
+	<body>
+		<h1>OpenSpaceGame</h1>
+		<form action="login.php" method="post">
+			<fieldset>
+				<legend>Login</legend>
+				<table>
+					<tr><th>Username:</th><td><input type="text" name="user" maxlength="32"></td></tr>
+					<tr><th>Passwort:</th><td><input type="password" name="pass" maxlength="32"></td></tr>
+					<tr><td colspan="2"><input type="reset" value="Clear"><input type="submit" value="Login"></td></tr>
+				</table>
+			</fieldset>
+		</form>
+		<form action="register.php" method="post">
+			<fieldset>
+				<legend>Register</legend>
+				<table>
+					<tr><th>Username:</th><td><input type="text" name="user" maxlength="32"></td></tr>
+					<tr><th>Passwort:</th><td><input type="password" name="pass" maxlength="32"></td></tr>
+					<tr><th>E-mail:</th><td><input type="text" name="email" maxlength="64"></td></tr>
+					<tr><td colspan="2"><input type="reset" value="Clear"><input type="submit" value="Register"></td></tr>
+				</table>
+			</fieldset>
+		</form>
+	</body>
+</html>
