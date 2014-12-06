78d7bf18cf69d98b5369deccb34bbd5be3c9fd26
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2014-12-06 10:51:56 +0200

    Added function to lookup translation.

--- base/php/class/trans.php
+++ base/php/class/trans.php
@@ -3,13 +3,39 @@ include_once("class/db.php");
 
 class trans
 {
-	public static function create($lang, $id, $name, $descr)
+	private $lang, $id, $name, $descr;
+
+	private function __construct(&$data)
 	{
-		db::create_object('translation', array(
-			'lang' => $lang,
-			'id' => $id,
-			'name' => $name,
-			'descr' => $descr));
+		$this->lang = $data['lang'];
+		$this->id = $data['id'];
+		$this->name = $data['name'];
+		$this->descr = $data['descr'];
+	}
+
+	public function __get($var)
+	{
+		switch($var)
+		{
+		case 'lang':
+			return $this->lang;
+		case 'id':
+			return $this->id;
+		case 'name':
+			return $this->name;
+		case 'descr':
+			return $this->descr;
+		default:
+			return null;
+		}
+	}
+
+	public static function find($lang, $id)
+	{
+		$data = db::select_one('translation', array('lang' => $lang, 'id' => $id));
+		if(!$data)
+			return false;
+		return new trans($data);
 	}
 }
 ?>
